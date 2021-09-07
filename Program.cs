bd5bbfc2bec85c21b99de4f17f6c0d134b6669e9
Michael Pineiro <pineiro@usf.edu>
2021-09-06 20:41:36 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StuckInATimeLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number between 1 and 100");            
+            
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            if (N >= 1 && N <= 100)
+            {
+                int i = 1;
+                while (i <= N)
+                {
+                    string note = i.ToString() + " Abracadabra";
+                    Console.WriteLine(note);
+                    i++;
+                }
+
+            }
+
+        }
+    }
+}
